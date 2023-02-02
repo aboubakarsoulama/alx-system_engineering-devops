22bc9b23901a81321005a66c0bfc551d6f5595a1
aboubakarsoulama <totypopito306@gmail.com>
2023-02-02 00:54:27 +0000

    2-recurse.py

--- 0x16-api_advanced/2-recurse.py
+++ 0x16-api_advanced/2-recurse.py
@@ -1,18 +1,19 @@
 #!/usr/bin/python3
-"""Function to recursively query hot posts in a given subreddit"""
+"""Contains recurse function"""
 import requests
 
 
-def recurse(subreddit, hot_list=[], after=''):
-    """ returns a list containing the titles
-    of all hot articles for a given subreddit"""
-    url = "https://www.reddit.com/r/{}/hot.json".format(subreddit)
+def recurse(subreddit, hot_list=[], after="", count=0):
+    """Returns a list of titles of all hot posts on a given subreddit."""
+    url = "https://www.reddit.com/r/{}/hot/.json".format(subreddit)
     headers = {
-        "User-Agent": "Mozilla/5.0 (X11; Ubuntu; Linux x86_\
-        64; rv:15.0) Gecko/20100101 Firefox/15.0.1"
+        "User-Agent": "0x16-api_advanced:project:\
+v1.0.0 (by /u/firdaus_cartoon_jr)"
     }
     params = {
-        "after": after
+        "after": after,
+        "count": count,
+        "limit": 100
     }
     response = requests.get(url, headers=headers, params=params,
                             allow_redirects=False)
@@ -21,9 +22,10 @@ def recurse(subreddit, hot_list=[], after=''):
 
     results = response.json().get("data")
     after = results.get("after")
-    for thread in results.get("children"):
-        hot_list.append(thread.get("data").get("title"))
+    count += results.get("dist")
+    for c in results.get("children"):
+        hot_list.append(c.get("data").get("title"))
 
     if after is not None:
-        return recurse(subreddit, hot_list, after)
+        return recurse(subreddit, hot_list, after, count)
     return hot_list
